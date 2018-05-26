18a70f578622d2965d68517a94dca12fadf58092
Konstantin <mijaeus@hotmail.com>
2018-05-26 12:42:01 -0400

    primer commit

---
+++ ejemplo.py
@@ -0,0 +1,11 @@
+#! C:\Program Files (x86)\Python
+# -*- coding: iso-8859-15 -*-
+import os, sys
+a = input ("Numero 1: ")
+b = input ("Numero 2: ")
+c = input ("Numero 3: ")
+d = input ("Numero 4: ")
+suma = (a+b+c+d)
+texto = "Sumatoria:"
+
+print texto, suma
