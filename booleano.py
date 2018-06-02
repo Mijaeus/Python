07dc41a9155a21c953c0ea67244acae0c565f932
Konstantin <mijaeus@hotmail.com>
2018-06-02 13:33:43 -0400

    Clase 2

---
+++ booleano.py
@@ -0,0 +1,8 @@
+#! C:\Program Files (x86)\Python
+# -*- coding: iso-8859-15 -*-
+import os, sys
+
+valor = 5
+a = int(input("Ingrese numero: "))
+if a==valor:
+    print 'correcto'
---
+++ edad.py
@@ -0,0 +1,8 @@
+#! C:\Program Files (x86)\Python
+# -*- coding: iso-8859-15 -*-
+import os, sys
+edad = int(input("Escribe tu edad: "))
+    if edad < 18
+printo ("Eres menor de edad :")
+    else :
+print ("eres menor de edad: ")
